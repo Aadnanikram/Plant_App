f575147bf6195f414005f21c377950126f0de12e
Aadnanikram <adnanikram199@gmail.com>
2021-06-28 13:42:26 +0500

    Add plant pic , price and adjust main screen

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
-import 'header_with_seachbox.dart';
+import 'package:plant_app/screens/home/components/featurred_plants.dart';
+import 'package:plant_app/screens/home/components/recomend_plants.dart';
+import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   @override
